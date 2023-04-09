bcbc65e9f3ee5cf7f389ccb7a44ab73f0766c47b
Chanakya <chanakya1patel@gmail.com>
2023-04-09 15:55:38 -0400

    prepared

---
+++ my-app/pages/index.tsx
@@ -0,0 +1,22 @@
+import Head from "next/head"
+import Link from "next/link"
+
+import { siteConfig } from "@/config/site"
+import { Layout } from "@/components/layout"
+import { buttonVariants } from "@/components/ui/button"
+
+export default function IndexPage() {
+  return (
+    <Layout>
+      <Head>
+        <title>Next.js</title>
+        <meta
+          name="description"
+          content="Next.js template for building apps with Radix UI and Tailwind CSS"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </Layout>
+  )
+}
